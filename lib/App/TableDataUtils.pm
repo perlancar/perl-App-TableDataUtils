af8e297a0e312382f7468c836dbef4c701eac11b
perlancar (@pc-home) <perlancar@gmail.com>
2015-09-13 13:53:39 +0700

    Rename functions/script: gen-table-data -> gen-rand-table

--- lib/App/TableDataUtils.pm
+++ lib/App/TableDataUtils.pm
@@ -9,7 +9,7 @@ use warnings;
 
 our %SPEC;
 
-$SPEC{gen_hash} = {
+$SPEC{gen_rand_hash} = {
     v => 1.1,
     summary => 'Generate hash with random keys/values',
     args => {
@@ -21,7 +21,7 @@ $SPEC{gen_hash} = {
         },
     },
 };
-sub gen_hash {
+sub gen_rand_hash {
     my %args = @_;
 
     my $hash = {};
@@ -37,7 +37,7 @@ sub gen_hash {
     [200, "OK", $hash];
 }
 
-$SPEC{gen_aos} = {
+$SPEC{gen_rand_aos} = {
     v => 1.1,
     summary => 'Generate array of scalars with random values',
     args => {
@@ -49,7 +49,7 @@ $SPEC{gen_aos} = {
         },
     },
 };
-sub gen_aos {
+sub gen_rand_aos {
     my %args = @_;
 
     my $aos = [];
@@ -60,7 +60,7 @@ sub gen_aos {
     [200, "OK", $aos];
 }
 
-$SPEC{gen_aoaos} = {
+$SPEC{gen_rand_aoaos} = {
     v => 1.1,
     summary => 'Generate array of (array of scalars) with random values',
     args => {
@@ -78,7 +78,7 @@ $SPEC{gen_aoaos} = {
         },
     },
 };
-sub gen_aoaos {
+sub gen_rand_aoaos {
     my %args = @_;
 
     my $aoaos = [];
@@ -93,7 +93,7 @@ sub gen_aoaos {
     [200, "OK", $aoaos];
 }
 
-$SPEC{gen_aohos} = {
+$SPEC{gen_rand_aohos} = {
     v => 1.1,
     summary => 'Generate array of (hash of scalars) with random values',
     args => {
@@ -111,14 +111,14 @@ $SPEC{gen_aohos} = {
         },
     },
 };
-sub gen_aohos {
+sub gen_rand_aohos {
     my %args = @_;
 
     my $aohos = [];
 
     my @columns;
     {
-        my $gen_hash_res = gen_hash(num_keys => $args{num_columns});
+        my $gen_hash_res = gen_rand_hash(num_keys => $args{num_columns});
         @columns = keys %{ $gen_hash_res->[2] };
     }
 
